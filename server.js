4ae30023d62d93be28c66bfbf3227be3c79abbe0
Симеон Мечков <montishak@gmail.com>
2019-11-14 01:57:01 +0200

    cors 11

--- server.js
+++ server.js
@@ -8,20 +8,8 @@ const PRIVATE_VAPID = 'hE5xhj7at5vKliWRGiV28hothLtCg1uYfBQcGFcoXNc';
 
 const fakeDatabase = [];
 const app = express();
-const allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    } else {
-        next();
-    }
-};
 app.use(cors());
-app.use(allowCrossDomain);
 app.use(bodyParser.json());
 
 webpush.setVapidDetails('mailto:monyou@abv.bg', PUBLIC_VAPID, PRIVATE_VAPID);
@@ -59,7 +47,7 @@ app.post('/SendPushNotification', (err, req, res) => {
         )
     })
 
-    Promise.all(promises).then(() => res.send('OK'));
+    Promise.all(promises).then(() => console.log(res));
 });
 
 app.listen(process.env.PORT || 5000, () => {
